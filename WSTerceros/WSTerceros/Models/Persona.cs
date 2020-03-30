3bf6679ad870fbc9f4fda9300cb25aaa6ba184a6
leonardolg05 <39814347+leonardolg05@users.noreply.github.com>
2020-03-30 11:48:42 -0500

    Terceros
    Servicio Terceros

---
+++ WSTerceros/WSTerceros/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSTerceros.Models
+{
+    public class Persona:Cliente
+    {
+        public string PrimerApellido { get; set; }
+        [StringLength(30)]
+        public string SegundoApellido { get; set; }
+        [StringLength(30)]
+        public string PrimerNombre { get; set; }
+        [StringLength(30)]
+        public string SegundoNombre { get; set; }
+        public string Sexo { get; set; }
+        public string EstadoCivil { get; set; }
+    }
+}
